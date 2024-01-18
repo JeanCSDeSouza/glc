ac6d12359c392717f06f75647ef9eb7d59bd7421
JeanCSDeSouza <jcsouza.first@gmail.com>
2024-01-18 05:52:23 -0300

    Reporitõrio e servicos de CrtcSnlAv

---
+++ src/main/java/br/com/bb/service/CrtcSnlAvService.java
@@ -0,0 +1,34 @@
+package br.com.bb.service;
+
+import br.com.bb.repository.CrtcSnlAvRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import br.com.bb.model.CrtcSnlAv;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class CrtcSnlAvService {
+    public CrtcSnlAvRepository crtcSnlAvRepository;
+    @Inject
+    public CrtcSnlAvService(CrtcSnlAvRepository crtcSnlAvRepository) {
+        this.crtcSnlAvRepository = crtcSnlAvRepository;
+    }
+
+    public List<CrtcSnlAv> getCrtcSnlAvs() {
+        return crtcSnlAvRepository.listAll();
+    }
+
+    @Transactional
+    public void addCrtcSnlAv(CrtcSnlAv crtcSnlAv) {
+        crtcSnlAvRepository.persist(crtcSnlAv);
+    }
+
+    @Transactional
+    public void deleteCrtcSnlAv(Long id) {
+        crtcSnlAvRepository.deleteById(id);
+    }
+
+}
